ece370bb9da4e397250fa820056970b943ab2d46
livesub <livesub@naver.com>
2021-08-23 10:07:18 +0900

    210823_01

--- resources/views/adm/email/emailmemberchk.blade.php
+++ resources/views/adm/email/emailmemberchk.blade.php
@@ -65,6 +65,10 @@
         }
 
         if (confirm("선택 하신 메일을 발송 하시겠습니까?") == true){    //확인
+            //메일 발송중 이미지 띄우기
+            LoadingWithMask('/img/loading.gif');
+            setTimeout("closeLoadingWithMask()", 300000);
+
             f.submit();
         }else{
             return;
@@ -72,6 +76,61 @@
     }
 </script>
 
+
+<style>
+    #loadingImg {
+        width: 100%;
+        height: 100%;
+        top: 0px;
+        left: 0px;
+        position: fixed;
+        display: block;
+        opacity: 0.7;
+        background-color: #fff;
+        z-index: 99;
+        text-align: center;
+    }
+
+    #loading-image {
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        z-index: 100;
+    }
+</style>
+
+
+<script>
+    function LoadingWithMask(gif) {
+        //화면의 높이와 너비를 구합니다.
+        var maskHeight = $(document).height();
+        var maskWidth  = window.document.body.clientWidth;
+
+        //화면에 출력할 마스크를 설정해줍니다.
+        var mask       = "<div id='mask' style='position:absolute; z-index:100; background-color:#000000; display:none; left:0; top:0;'></div>";
+        var loadingImg = '';
+
+        loadingImg += "<div id='loadingImg' style='width:700px;height:200px; text-align: center'>";
+        loadingImg += " <img src='"+gif+"' id='loading-image' style='position: relative; display: block; margin: 0px auto;'/>";
+        loadingImg += "</div>";
+
+        //화면에 레이어 추가
+        $('body').append(mask).append(loadingImg)
+
+        //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
+        $('#mask').css({
+                'width' : maskWidth
+                , 'height': maskHeight
+                , 'opacity' : '0.3'
+        });
+
+        //마스크 표시
+        $('#mask').show();
+
+        //로딩중 이미지 표시
+        $('#loadingImg').show();
+    }
+</script>
 
 
 
