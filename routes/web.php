05cb464c26d66a6be0937ff31b08aaaf66d7547b
livesub <livesub@naver.com>
2021-09-01 17:02:37 +0900

    210901_04

--- routes/web.php
+++ routes/web.php
@@ -243,8 +243,8 @@ Route::get('/item/item_page', [
 
 /*** 소셜 로그인 관련 ***/
 //구글 관련
-Route::get('social/{provider}', [
-    'as' => 'social.login',
+Route::get('social/google', [
+    'as' => 'google.login',
     'uses' => 'App\Http\Controllers\auth\socialLoginController@redirect',
 ]);
 
